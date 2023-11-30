b42689147829d94d52c4efd7cb6c0d39baa9ebef
Sudhanshu_angral <147260436+sudhanshuangral@users.noreply.github.com>
2023-11-30 22:09:20 +0530

    experiment2(a).c
    program for arthematic operations.

---
+++ experiment2(a).c
@@ -0,0 +1,48 @@
+//program for arthematic operations.
+
+(i) program for addition operator.
+#include<stdio.h>
+void main()
+{
+int num1,num2,sum;
+printf("Enter the two numbers to be added:"\n);
+scanf("%d%d",&num1,&num2);
+sum=num1+num2;
+printf("%d+%d=%d",sum);
+}
+
+
+(ii) program for subtraction operator.
+#include<stdio.h>
+void main()
+{
+ int num1,num2,diff;
+printf("Enter the two numbers to be subtracted:"\n);
+scanf("%d%d",&num1,&num2);
+diff=num1-num2;
+printf("%d-%d=%d");
+}
+
+
+(iii) program for multiplication operator.
+#include<stdio.h>
+void main()
+{
+int num1,num2,product;
+printf("Enter the two numbers to be multiplied:"\n);
+scanf("%d%d",&num1,&num2);
+product=num1*num2;
+printf("%d*%d=%d",product);
+}
+
+(iv) program for division operator.
+#include<stdio.h>
+void main()
+{
+int num1,num2,divide;
+printf("Enter the two numbers to be divided:"\n);
+scanf("%d%d",&num1,&num2);
+product=num1/num2;
+printf("%d/%d=%d",divide);
+}
+
