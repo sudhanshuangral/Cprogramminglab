175e08fd42f439c30a716d23195f18b4f86b8b94
Sudhanshu_angral <147260436+sudhanshuangral@users.noreply.github.com>
2023-11-30 22:07:04 +0530

    experiment1.c
    program for hello world.

---
+++ experiment1.c
@@ -0,0 +1,22 @@
+//program for hello world.
+#include<stdio.h>
+void main()
+{
+printf("Hello world");
+}
+
+
+//program for hello world using new tab operator.
+#include<stdio.h>
+void main()
+{
+printf("Hello world\t hello world");
+}
+
+
+//program for hello world using new line operator.
+#include<stdio.h>
+void main()
+{
+printf("Hello world/n hello world");
+}
