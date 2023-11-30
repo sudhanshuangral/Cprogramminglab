21d608d90f1491cce6b3b32adcb7e48c974c17e5
Sudhanshu_angral <147260436+sudhanshuangral@users.noreply.github.com>
2023-11-30 22:11:56 +0530

    experiment3(a).c
    program to find if the number is even or odd.

---
+++ experiment3(a).c
@@ -0,0 +1,16 @@
+//program to find if the number is even or odd.
+#include<stdio.h>
+void main()
+{
+int a;
+printf("Enter a number:");
+scanf("%d",&a);
+if(a%2==0)
+{
+printf("Number is even.");
+}
+else
+{
+printf("Number is odd.");
+}
+}
